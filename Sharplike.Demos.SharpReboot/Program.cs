fea3f8622d8b1be12dcfdaac386adeb89e84be30
Nicholas Rodine <halfofastaple@gmail.com>
2014-06-04 20:06:53 -0400

    Updated everything to .Net 4.0 Started adding a new Demo project, to help me figure out this library.

---
+++ Sharplike.Demos.SharpReboot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sharplike.Demos.SharpReboot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Game.Initialize();
+			
+			//Game.SetRenderSystem("OpenTK");
+			//Game.CreateRenderWindow(...);
+			
+			//Game.SetInputSystem("OpenTK");
+			//Game.SetAudioSystem("OpenTK");
+			
+			//g.Run(new StepwiseGameLoop(new StepwiseGameLoop.Execute(MyGame)));
+			
+			//g.Terminate();
+		}
+	}
+}
