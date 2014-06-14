7008e625f3a789c8ae02eac8e34a841f962d7748
Nicholas Rodine <halfofastaple@gmail.com>
2014-06-13 22:28:53 -0400

    Rolled version number of the Frontend dll, and added XML documentation.

--- Sharplike.Frontend.TK/Rendering/TKWindow.cs
+++ Sharplike.Frontend.TK/Rendering/TKWindow.cs
@@ -24,6 +24,9 @@ namespace Sharplike.Frontend.Rendering
 		private TKForm form;
 		private int paletteId;
 
+		/// <summary>
+		/// The inner GLControl responsible for rendering.
+		/// </summary>
 		public readonly TKGLControl Control = new TKGLControl();
 
 		public TKWindow(Size displayDimensions, GlyphPalette palette, Control context)
@@ -188,11 +191,17 @@ namespace Sharplike.Frontend.Rendering
 			GL.EndList();
 		}
 
+		/// <summary>
+		/// Changes the form text to the WindowTitle property.
+		/// </summary>
 		protected override void WindowTitleChange()
 		{
 			form.Text = this.WindowTitle;
 		}
 
+		/// <summary>
+		/// Updates the GLControl, and any subregions.
+		/// </summary>
 		public override void Update()
 		{
 			base.Update();
